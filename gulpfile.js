196836f63046369aeb0c92bfeb6813b31f913e12
Jamila90 <jamila.shakurova@gmail.com>
2020-10-28 00:32:15 +0400

    Исправила замечания по верстке

--- gulpfile.js
+++ gulpfile.js
@@ -9,9 +9,6 @@ const autoprefixer = require('autoprefixer');
 const server = require('browser-sync').create();
 const csso = require('gulp-csso');
 const rename = require('gulp-rename');
-const imagemin = require('gulp-imagemin');
-const webp = require('gulp-webp');
-const svgstore = require('gulp-svgstore');
 const posthtml = require('gulp-posthtml');
 const include = require('posthtml-include');
 const del = require('del');
@@ -41,7 +38,6 @@ gulp.task('server', function () {
   });
 
   gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css', 'refresh'));
-  gulp.watch('source/img/icon-*.svg', gulp.series('sprite', 'html', 'refresh'));
   gulp.watch('source/*.html', gulp.series('html', 'refresh'));
   gulp.watch('source/js/*.js', gulp.series('copy', 'refresh'));
 });
@@ -49,31 +45,6 @@ gulp.task('server', function () {
 gulp.task('refresh', function (done) {
   server.reload();
   done();
-});
-
-gulp.task('images', function () {
-  return gulp.src('source/img/**/*.{png,jpg,svg}')
-      .pipe(imagemin([
-        imagemin.optipng({optimizationLevel: 3}),
-        imagemin.jpegtran({progressive: true}),
-        imagemin.svgo()
-      ]))
-
-      .pipe(gulp.dest('source/img'));
-
-});
-
-gulp.task('webp', function () {
-  return gulp.src('source/img/**/*.{png,jpg}')
-      .pipe(webp({quality: 90}))
-      .pipe(gulp.dest('source/img'));
-});
-
-gulp.task('sprite', function () {
-  return gulp.src('source/img/{icon-*,htmlacademy*}.svg')
-      .pipe(svgstore({inlineSvg: true}))
-      .pipe(rename('sprite_auto.svg'))
-      .pipe(gulp.dest('build/img'));
 });
 
 gulp.task('html', function () {
@@ -100,5 +71,5 @@ gulp.task('clean', function () {
   return del('build');
 });
 
-gulp.task('build', gulp.series('clean', 'copy', 'css', 'sprite', 'html'));
+gulp.task('build', gulp.series('clean', 'copy', 'css', 'html'));
 gulp.task('start', gulp.series('build', 'server'));
